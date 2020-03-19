a71edb9f569b732804e9bcf1685c3295268d884b
wai-calvin <cwai14@live.com>
2020-03-18 23:21:24 -0700

    new updates

--- src/components/form.js
+++ src/components/form.js
@@ -1,16 +1,21 @@
 import React, {Component} from 'react';
 
 class Form extends Component {
+    constructor(props) {
+        super();
+    }
+
   render() {
     return (
       <div>
-        <form>
+        <h1>Hello, {this.props.name}</h1>
+        {/* <form>
             <label>
                 Name:
                 <input type="text" name="name" />
             </label>
             <input type="submit" value="Submit" />
-        </form>
+        </form> */}
       </div>
     );
   }
