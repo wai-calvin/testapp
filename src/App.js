2c19cc2485e139f378ef366894fc2aefe25e7100
wai-calvin <cwai14@live.com>
2020-03-18 23:11:49 -0700

    form

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Form from './components/form';
 
 class App extends Component {
   render() {
     return (
       <div>
-        hello
+        <Form />
       </div>
     );
   }
